41e8be0cae4b72b0e87398732dea625e486e9b47
mjherraiz <mariajoenlared@gmail>
2019-04-10 18:57:37 +0200

    linters

--- js/index.js
+++ js/index.js
@@ -1,21 +1,21 @@
 function loadScript(src) {
-  return new Promise(function(resolve, reject) {
-    let script = document.createElement("script");
-    script.src = src;
+    return new Promise(function(resolve, reject) {
+        let script = document.createElement('script');
+        script.src = src;
 
-    script.onload = () => resolve(script); 
-    script.onerror = () => reject(new Error("Script load error: " + src));
+        script.onload = () => resolve(script);
+        script.onerror = () => reject(new Error('Script load error: ' + src));
 
-    document.head.append(script); 
-  });
+        document.head.append(script);
+    });
 }
 let promise = loadScript(
-"https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.11/lodash.js"
+    'https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.11/lodash.js'
 );
 
 promise.then(
-  script => alert(`${script.src} is loaded!`),
-  error => alert(`Error: ${error.message}`)
+    script => alert(`${script.src} is loaded!`),
+    error => alert(`Error: ${error.message}`)
 );
 
-promise.then(script => alert("One more handler to do something else!"));
+promise.then(script => alert('One more handler to do something else!'));
